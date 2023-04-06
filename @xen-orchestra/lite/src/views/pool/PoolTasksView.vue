ee330e34eb92512162e2544d467c5182379d17cf
Rajaa <rajaa.barhtaoui@gmail.com>
2023-04-06 04:45:11 +0200

    feat(lite/pool/tasks): display an error msg if data cannot be fetched

--- @xen-orchestra/lite/src/views/pool/PoolTasksView.vue
+++ @xen-orchestra/lite/src/views/pool/PoolTasksView.vue
@@ -1,12 +1,10 @@
 <template>
-  <UiCard>
+  <UiCard :color="hasError ? 'error' : undefined">
     <UiTitle class="title-with-counter" type="h4">
       {{ $t("tasks") }}
-      <UiCounter :value="pendingTasks.length" color="info" />
+      <UiCounter :value="pendingTasks?.length ?? 0" color="info" />
     </UiTitle>
-
     <TasksTable :pending-tasks="pendingTasks" :finished-tasks="finishedTasks" />
-    <UiSpinner class="loader" v-if="!isReady" />
   </UiCard>
 </template>
 
@@ -14,7 +12,6 @@
 import TasksTable from "@/components/tasks/TasksTable.vue";
 import UiCard from "@/components/ui/UiCard.vue";
 import UiCounter from "@/components/ui/UiCounter.vue";
-import UiSpinner from "@/components/ui/UiSpinner.vue";
 import UiTitle from "@/components/ui/UiTitle.vue";
 import useArrayRemovedItemsHistory from "@/composables/array-removed-items-history.composable";
 import useCollectionFilter from "@/composables/collection-filter.composable";
@@ -25,39 +22,48 @@ import type { XenApiTask } from "@/libs/xen-api";
 import { useTaskStore } from "@/stores/task.store";
 import { useTitle } from "@vueuse/core";
 import { storeToRefs } from "pinia";
-import { computed } from "vue";
+import { computed, watch, type ComputedRef, type Ref } from "vue";
 import { useI18n } from "vue-i18n";
 
-const { allRecords, isReady } = storeToRefs(useTaskStore());
+const { allRecords, hasError, isReady } = storeToRefs(useTaskStore());
 const { t } = useI18n();
 
-const { compareFn } = useCollectionSorter<XenApiTask>({
-  initialSorts: ["-created"],
-});
+let pendingTasks: ComputedRef<XenApiTask[]> | undefined,
+  finishedTasks: Ref | undefined;
 
-const allTasks = useSortedCollection(allRecords, compareFn);
+watch(isReady, () => {
+  const { compareFn } = useCollectionSorter<XenApiTask>({
+    initialSorts: ["-created"],
+  });
 
-const { predicate } = useCollectionFilter({
-  initialFilters: ["!name_label:|(SR.scan host.call_plugin)", "status:pending"],
-});
+  const allTasks = useSortedCollection(allRecords, compareFn);
+  const { predicate } = useCollectionFilter({
+    initialFilters: [
+      "!name_label:|(SR.scan host.call_plugin)",
+      "status:pending",
+    ],
+  });
 
-const pendingTasks = useFilteredCollection<XenApiTask>(allTasks, predicate);
+  pendingTasks = useFilteredCollection<XenApiTask>(allTasks, predicate);
 
-const finishedTasks = useArrayRemovedItemsHistory(
-  allTasks,
-  (task) => task.uuid,
-  {
+  finishedTasks = useArrayRemovedItemsHistory(allTasks, (task) => task.uuid, {
     limit: 50,
     onRemove: (tasks) =>
       tasks.map((task) => ({
         ...task,
         finished: new Date().toISOString(),
       })),
-  }
-);
+  });
+  return;
+});
 
 useTitle(
-  computed(() => t("task.page-title", { n: pendingTasks.value.length }))
+  computed(() => {
+    const tasks = pendingTasks?.value;
+    return t("task.page-title", {
+      n: tasks === undefined ? 0 : tasks.length,
+    });
+  })
 );
 </script>
 
@@ -71,12 +77,5 @@ useTitle(
   .ui-counter {
     font-size: 1.4rem;
   }
-}
-
-.loader {
-  color: var(--color-extra-blue-base);
-  display: block;
-  font-size: 4rem;
-  margin: 2rem auto 0;
 }
 </style>
